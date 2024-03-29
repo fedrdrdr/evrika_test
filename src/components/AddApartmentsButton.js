c1c02425d12b5e4e582380fe9805d8f787a3b73a
Fedor <fkalit@evvve.net>
2024-03-29 13:29:22 +0300

    fix styles and code

--- src/components/AddApartmentsButton.js
+++ src/components/AddApartmentsButton.js
@@ -1,6 +1,7 @@
 // AddApartmentsButton.js
 import React, { useState } from 'react';
 import Modal from './Modal';
+import AddIcon from '../icons/AddIcon';
 
 function AddApartmentsButton({ addApartments }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,8 +11,8 @@ function AddApartmentsButton({ addApartments }) {
   };
 
   return (
-    <div className="add-apartments-button">
-      <button onClick={toggleModal}>Добавить</button>
+    <div >
+      <button onClick={toggleModal} className="button"><AddIcon /></button>
       {isOpen && <Modal addApartments={addApartments} toggleModal={toggleModal} />}
     </div>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -4,52 +4,43 @@ import Table from './Table';
 import AddApartmentsButton from './AddApartmentsButton';
 
 function App() {
-  const [apartments1, setApartments1] = useState([]);
-  const [apartments2, setApartments2] = useState([]);
-  const [apartments3, setApartments3] = useState([]);
-  const [apartments4, setApartments4] = useState([]);
+  const [houses, setHouses] = useState([
+    { number: 1, apartments: [] },
+    { number: 2, apartments: [] },
+    { number: 3, apartments: [] },
+    { number: 4, apartments: [] },
+  ]);
 
-  const clearApartments = (setApartments) => {
-    setApartments([]);
+  const clearApartments = (houseNumber) => {
+    setHouses(prevHouses => prevHouses.map(house => {
+      if (house.number === houseNumber) {
+        return { ...house, apartments: [] };
+      }
+      return house;
+    }));
+  };
+
+  const addApartments = (houseNumber, selectedApartments) => {
+    setHouses(prevHouses => prevHouses.map(house => {
+      if (house.number === houseNumber) {
+        return { ...house, apartments: [...house.apartments, ...selectedApartments] };
+      }
+      return house;
+    }));
   };
 
   return (
     <div className="app">
-      <div className="block">
-        <div className="table-header ">
-          <h2>Дом: 1</h2>
-          <AddApartmentsButton addApartments={(selectedApartments) => setApartments1([...apartments1, ...selectedApartments])} />
-          <button onClick={() => clearApartments(setApartments1)}>Очистить</button>
+      {houses.map(house => (
+        <div key={house.number} className="block">
+          <div className="table-header">
+            <h2>Дом: {house.number}</h2>
+            <AddApartmentsButton addApartments={(selectedApartments) => addApartments(house.number, selectedApartments)} />
+            <button onClick={() => clearApartments(house.number)}>Очистить</button>
+          </div>
+          <Table apartments={house.apartments} houseNumber={house.number} clearApartments={() => clearApartments(house.number)} />
         </div>
-        <Table apartments={apartments1} houseNumber={1} clearApartments={() => clearApartments(setApartments1)} />
-      </div>
-      
-      <div className="block">
-        <div className="table-header ">
-          <h2>Дом: 2</h2>
-          <AddApartmentsButton addApartments={(selectedApartments) => setApartments2([...apartments2, ...selectedApartments])} />
-          <button onClick={() => clearApartments(setApartments2)}>Очистить</button>
-        </div>
-        <Table apartments={apartments2} houseNumber={2} clearApartments={() => clearApartments(setApartments2)} />
-      </div>
-      
-      <div className="block">
-        <div className="table-header ">
-          <h2>Дом: 3</h2>
-          <AddApartmentsButton addApartments={(selectedApartments) => setApartments3([...apartments3, ...selectedApartments])} />
-          <button onClick={() => clearApartments(setApartments3)}>Очистить</button>
-        </div>
-        <Table apartments={apartments3} houseNumber={3} clearApartments={() => clearApartments(setApartments3)} />
-      </div>
-      
-      <div className="block">
-        <div className="table-header ">
-          <h2>Дом: 4</h2>
-          <AddApartmentsButton addApartments={(selectedApartments) => setApartments4([...apartments4, ...selectedApartments])} />
-          <button onClick={() => clearApartments(setApartments4)}>Очистить</button>
-        </div>
-        <Table apartments={apartments4} houseNumber={4} clearApartments={() => clearApartments(setApartments4)} />
-      </div>
+      ))}
     </div>
   );
 }
